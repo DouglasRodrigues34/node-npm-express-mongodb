322995cef9f580ac4fc8cb207e5e8d472eff8822
DonMGameR <donmoutsystems.js@gmail.com>
2023-03-01 07:11:57 -0300

    aula 01 nodeJS

---
+++ aula-01/app.js
@@ -0,0 +1,10 @@
+// const mod1 = require('./mod1')
+// const falaNome = mod1.falaNome
+
+// console.log(mod1.falaNome())
+
+const { Pessoa } = require('./mod1')
+
+const p1 = new Pessoa('Douglas')
+
+console.log(p1)
---
+++ aula-01/mod1.js
@@ -0,0 +1,22 @@
+// const nome = 'Douglas'
+// const sobrenome = 'Rodrigues'
+
+// const falaNome = () => nome + ' ' + sobrenome 
+
+// // module.exports.nome = nome
+// // module.exports.sobrenome = sobrenome
+// // // module.exports.falaNome = falaNome
+
+// exports.nome = nome
+// exports.sobrenome = sobrenome
+// exports.falaNome = falaNome
+// this.qualquerCoisa = 'O que eu quiser exportar'
+
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+}
+
+exports.Pessoa = Pessoa

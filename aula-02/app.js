b17588c6dd94865cfda06d0bddbccfec8f7a6e7b
DonMGameR <donmoutsystems.js@gmail.com>
2023-03-02 07:19:50 -0300

    aula 02 node modulo

---
+++ aula-02/app.js
@@ -0,0 +1,11 @@
+// const multiplicacao = require('./mod')
+
+// console.log(multiplicacao(2, 2))
+
+// const cachorro = require('./mod')
+
+// const cachorrinho = new cachorro('Dog')
+// cachorrinho.latir()
+
+const path = require('path')
+console.log(path.resolve(__dirname, '..', '..'))
